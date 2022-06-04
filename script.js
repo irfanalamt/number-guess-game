dc3bdfceed0277d44d764037144cf95afb283911
Irfan <irfanalamt@gmail.com>
2022-06-04 15:12:47 -0400

    Feat: check if guess is empty, display error

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+  }
+});
