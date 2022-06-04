f8e544576b10aa49fb9d9adf70c0214a747262ed
Irfan <irfanalamt@gmail.com>
2022-06-04 15:57:54 -0400

    Fix: hide random number

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
-//Display secret number after generation
-document.querySelector('.number').textContent = secretNumber;
-
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
