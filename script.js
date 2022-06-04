3f8db66e141dd984456e44a5c6648059681c43c2
Irfan <irfanalamt@gmail.com>
2022-06-04 15:39:19 -0400

    Feat: add game logic

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+//Display secret number after generation
+document.querySelector('.number').textContent = secretNumber;
+
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
@@ -12,5 +15,23 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
     displayMessage('⛔️ No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
